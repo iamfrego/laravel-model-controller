53d8468e8a7e4bbb694f945931140aa9fbe3e6cb
iamfrego <mate_freg@tiscali.it>
2022-01-11 15:59:33 +0100

    Css Add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,26 @@
 
 @section('content')
     <div class="container">
-        <h1>My Movie</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo praesentium possimus rem nulla. Dolor possimus ea vel sed suscipit ut beatae.</p>
-
-
-        <section class="books">
+        <section class="movies">
             
-
             @forelse ($movies as $movie)
                 <div class="movie">
-                    <h4>{{$movie->title}}</h4>
-                    <h5>{{$movie->original_title}}</h5>
-                    <div class="description">
-                        <p>{{$movie->nationality}}</p>
-                        <p>{{$movie->date}}</p>
-                        <p>{{$movie->vote}}</p>
-
+                    <div class="badge">
+                        <p>{{$movie->id}}</p>
                     </div>
 
+                   @if ($movie->title == $movie->original_title)
+                       <h4 class="title">Title: {{$movie->title}}</h4>
+                   @else
+                    <h4 class="title">Title: {{$movie->title}}</h4>
+                    <h5 class="og_title">Original title: {{$movie->original_title}}</h5>
+                   @endif
+                    
+                   <p class="nationality">Nationality: {{$movie->nationality}}</p>
+                   <div class="description">
+                        <p class="date">Date: {{$movie->date}}</p>
+                        <p class="vote">Vote: {{$movie->vote}}</p>
+                    </div>
                 </div>
             @empty
                 <p>Sorry, no movie aviables now</p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     </head>
     <body>
         @yield('content')
