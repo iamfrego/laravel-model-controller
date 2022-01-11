7c1df9a94e0d5febba9f043e33b0918da9dd819b
iamfrego <mate_freg@tiscali.it>
2022-01-11 15:23:28 +0100

    Show Database data in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app');
 
 @section('content')
-    <h1>home</h1>
+    <div class="container">
+        <h1>My Movie</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo praesentium possimus rem nulla. Dolor possimus ea vel sed suscipit ut beatae.</p>
+
+
+        <section class="books">
+            
+
+            @forelse ($movies as $movie)
+                <div class="movie">
+                    <h4>{{$movie->title}}</h4>
+                    <h5>{{$movie->original_title}}</h5>
+                    <div class="description">
+                        <p>{{$movie->nationality}}</p>
+                        <p>{{$movie->date}}</p>
+                        <p>{{$movie->vote}}</p>
+
+                    </div>
+
+                </div>
+            @empty
+                <p>Sorry, no movie aviables now</p>
+            @endforelse
+        </section>
+
+    </div>
 @endsection
